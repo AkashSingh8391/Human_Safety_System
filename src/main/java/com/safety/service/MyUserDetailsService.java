5639ec438d31856c1447291958dc1ad5c542133b
Akash Singh <akashgurjar6266@gmail.com>
2025-11-29 22:23:52 +0530

    Update authentication

--- src/main/java/com/safety/service/MyUserDetailsService.java
+++ src/main/java/com/safety/service/MyUserDetailsService.java
@@ -1,5 +1,4 @@
 package com.safety.service;
-
 import com.safety.model.User;
 import com.safety.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,10 @@ import java.util.Collections;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
     @Autowired private UserRepository userRepo;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User u = userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(
-                u.getUsername(),
-                u.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + (u.getRole()==null?"CIVIL":u.getRole())))
-        );
+        User u = userRepo.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(u.getUsername(), u.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
     }
 }
