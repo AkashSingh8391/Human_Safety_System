35623cf6a912c6cf437348e29be070936c007e7a
Akash Singh <akashgurjar6266@gmail.com>
2025-11-29 22:52:55 +0530

    Update Reponse class

--- src/main/java/com/safety/dto/AuthRequest.java
+++ src/main/java/com/safety/dto/AuthRequest.java
@@ -1,18 +1,34 @@
 package com.safety.dto;
-
 public class AuthRequest {
     private String username;
     private String password;
-    private String phoneNo;
-    private String email;
-    // getters/setters
-    public AuthRequest(){}
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getPhoneNo() { return phoneNo; }
-    public void setPhoneNo(String phoneNo) { this.phoneNo = phoneNo; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    private String emergencyEmail;
+    private String emergencyPhone;
+    // getters & setters
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmergencyEmail() {
+		return emergencyEmail;
+	}
+	public void setEmergencyEmail(String emergencyEmail) {
+		this.emergencyEmail = emergencyEmail;
+	}
+	public String getEmergencyPhone() {
+		return emergencyPhone;
+	}
+	public void setEmergencyPhone(String emergencyPhone) {
+		this.emergencyPhone = emergencyPhone;
+	}
+    
+    
 }
