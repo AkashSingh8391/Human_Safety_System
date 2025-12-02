a16203c56351ff714147b3cab8fa7527266470a8
Akash Singh <akashgurjar6266@gmail.com>
2025-12-03 02:16:20 +0530

    Same as Prev

--- src/main/java/com/safety/dto/AuthRequest.java
+++ src/main/java/com/safety/dto/AuthRequest.java
@@ -4,20 +4,9 @@ public class AuthRequest {
     private String email;
     private String password;
 
-    // Registration fields
-    private String contactEmail;
-    private String contactPhone;
-
-    // Getters & setters
+    public AuthRequest() {}
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
-    public String getContactEmail() { return contactEmail; }
-    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
-
-    public String getContactPhone() { return contactPhone; }
-    public void setContactPhone(String contactPhone) { this.contactPhone = contactPhone; }
 }
--- src/main/java/com/safety/dto/AuthResponse.java
+++ src/main/java/com/safety/dto/AuthResponse.java
@@ -5,6 +5,4 @@ public class AuthResponse {
     public AuthResponse(String token) { this.token = token; }
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
-    
-    
 }
