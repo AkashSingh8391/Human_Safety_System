fe338e9fe7dcc42959210105974736b192396e59
Akash Singh <akashgurjar6266@gmail.com>
2025-12-03 02:14:48 +0530

    Update a AlertRepo

--- src/main/java/com/safety/repository/EmergencyContactRepository.java
+++ src/main/java/com/safety/repository/EmergencyContactRepository.java
@@ -2,9 +2,8 @@ package com.safety.repository;
 
 import com.safety.model.EmergencyContact;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface EmergencyContactRepository extends JpaRepository<EmergencyContact, Long> {
-    List<EmergencyContact> findByUserUserId(Long userId);
+    List<EmergencyContact> findByUserId(Long userId);
 }
