385a04b9fb561ebeed417e7264c493f48465fb55
Akash Singh <akashgurjar6266@gmail.com>
2025-12-02 23:31:52 +0530

    Update MyUserDetailsService to load by email

--- src/main/java/com/safety/service/MyUserDetailsService.java
+++ src/main/java/com/safety/service/MyUserDetailsService.java
@@ -1,4 +1,5 @@
 package com.safety.service;
+
 import com.safety.model.User;
 import com.safety.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,11 @@ import java.util.Collections;
 public class MyUserDetailsService implements UserDetailsService {
     @Autowired private UserRepository userRepo;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User u = userRepo.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(u.getUsername(), u.getPassword(),
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        // we treat username parameter as email now
+        User u = userRepo.findByEmail(usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(u.getEmail(), u.getPassword(),
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
     }
 }
